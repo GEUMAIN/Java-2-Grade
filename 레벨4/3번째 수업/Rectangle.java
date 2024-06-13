17ccc0353fa1d2299bc8422e140972b1c4939b2f
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2024-06-13 18:37:34 +0900

    Add files via upload

---
+++ 레벨4/3번째 수업/Rectangle.java
@@ -0,0 +1,27 @@
+package java_level4.lang.object.test;
+
+public class Rectangle {
+	private int width;
+	private int height;
+	
+	public Rectangle(int width,int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	@Override
+	public String toString() {
+		return "Rectangle{" +
+				"가로=" + width +
+				", 세로=" + height +
+				'}';
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Rectangle rectangle = (Rectangle) o;
+		return width == rectangle.width && height == rectangle.height;
+	}
+}
