331783f4da9166693297aeee287104234784aa11
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2024-06-25 17:39:36 +0900

    Add files via upload

---
+++ 레벨4/7번째 수업/WrapperClassMain.java
@@ -0,0 +1,11 @@
+package java_level4.lang.wrapper;
+
+public class WrapperClassMain {
+	public static void main(String[] args) {
+		Integer integerObj = Integer.valueOf(10);
+		Integer newInteger = 10;
+		
+		Long newLong = 100L;
+		
+	}
+}
