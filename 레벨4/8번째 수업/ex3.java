4d453a337ad3b1f09d43561d953d7160afeddc33
jeong in seo <128437656+GEUMAIN@users.noreply.github.com>
2024-06-26 17:42:04 +0900

    Add files via upload

---
+++ 레벨4/8번째 수업/ex3.java
@@ -0,0 +1,15 @@
+package java_level4.lang.wrapper.ex;
+
+import java.util.Scanner;
+
+public class ex3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("케이스 개수 입력 : ");
+		int N = sc.nextInt();
+		int[] Case = new int[N];
+		
+	
+	}
+}
